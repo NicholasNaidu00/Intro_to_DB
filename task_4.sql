545c3c582b2dd2460ce2f0c7fb860e0f83a6174e
Nicholas Naidu <nicholasnaidu@ymail.com>
2025-01-31 11:43:08 +0200

    Added Task 4

---
+++ task_4.sql
@@ -0,0 +1,4 @@
+-- Print the full description of the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'Books';
