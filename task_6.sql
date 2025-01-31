3d0cbb2036e2139db7435ee9b65da9062e924e9f
Nicholas Naidu <nicholasnaidu@ymail.com>
2025-01-31 11:54:19 +0200

    Added Remaining Tasks

---
+++ task_6.sql
@@ -0,0 +1,8 @@
+-- Use the database
+USE alx_book_store;
+
+-- Insert multiple rows into the Customers table
+INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
+(5, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(6, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(7, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
